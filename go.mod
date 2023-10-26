87b028940df298f5b54536107768861482cc2fc9
Mani Movassagh <mani.mgh@gmail.com>
2023-10-26 20:01:22 +0200

    first

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/manimovassagh/command-app
+
+go 1.21.3
---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func updateCommitPush() {
+	// Command 1: Add all files recursively to git repo
+	addCmd := exec.Command("git", "add", "-A")
+	addCmd.Stderr = os.Stderr
+	addCmd.Stdout = os.Stdout
+	err := addCmd.Run()
+	if err != nil {
+		fmt.Println("Error: Failed to execute git add command")
+		os.Exit(1)
+	}
+
+	// Prompt the user for a commit message
+	fmt.Print("Enter a commit message: ")
+	var commitMsg string
+	fmt.Scanln(&commitMsg)
+
+	// Command 2: Commit all changes with the user's provided message
+	commitCmd := exec.Command("git", "commit", "-m", commitMsg)
+	commitCmd.Stderr = os.Stderr
+	commitCmd.Stdout = os.Stdout
+	err = commitCmd.Run()
+	if err != nil {
+		fmt.Println("Error: Failed to execute git commit command")
+		os.Exit(1)
+	}
+
+	// Command 3: Push to remote (origin master)
+	pushCmd := exec.Command("git", "push", "origin", "master")
+	pushCmd.Stderr = os.Stderr
+	pushCmd.Stdout = os.Stdout
+	err = pushCmd.Run()
+	if err != nil {
+		fmt.Println("Error: Failed to execute git push command")
+		os.Exit(1)
+	}
+
+	fmt.Println("Successfully added, committed, and pushed changes!")
+}
+
+func main() {
+	updateCommitPush()
+}
